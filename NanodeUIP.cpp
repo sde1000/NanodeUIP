cfb06ce4eafcb9b7109e1f55914bd4bc72a926d7
maniacbug <maniacbug@ymail.com>
2012-01-02 15:43:46 -0800

    Move NanodeUIP sprintf's into progmem

--- NanodeUIP.cpp
+++ NanodeUIP.cpp
@@ -93,14 +93,14 @@ void NanodeUIP::set_nameserver_addr(byte a, byte b, byte c, byte d) {
 
 // Requires a buffer of at least 18 bytes to format into
 void NanodeUIP::get_mac_str(char *buf) {
-  sprintf(buf,"%02X:%02X:%02X:%02X:%02X:%02X",
+  sprintf_P(buf,PSTR("%02X:%02X:%02X:%02X:%02X:%02X"),
 	  uip_ethaddr.addr[0], uip_ethaddr.addr[1], uip_ethaddr.addr[2],
 	  uip_ethaddr.addr[3], uip_ethaddr.addr[4], uip_ethaddr.addr[5]);
 }
 
 // Requires a buffer of at least 16 bytes to format into
 void NanodeUIP::format_ipaddr(char *buf,uint16_t *addr) {
-  sprintf(buf,"%d.%d.%d.%d",addr[0]&0xff,addr[0]>>8,
+  sprintf_P(buf,PSTR("%d.%d.%d.%d"),addr[0]&0xff,addr[0]>>8,
 	  addr[1]&0xff,addr[1]>>8);
 }
 
